d515f6ab3c39f0db45c8fcdb8184d4b32f91f1aa
Ludvig Noring <ludvig.noring@gmail.com>
2025-11-19 14:50:52 +0100

    Update backend URL to videoconfbackend2

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
 // Configuration for production deployment
-window.BACKEND_URL = 'wss://team1-videoconfbackend.eyevinn-web-runner.auto.prod.osaas.io';
+window.BACKEND_URL = 'wss://team1-videoconfbackend2.eyevinn-web-runner.auto.prod.osaas.io';
